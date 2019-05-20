03634296993c06bfb892bf535946d21c50841b23
habenhishe <49343874+habenhishe@users.noreply.github.com>
2019-05-20 09:51:28 -0700

    Update module07.js

--- assignments/module07.js
+++ assignments/module07.js
@@ -8,12 +8,10 @@ function Hotel(name, rooms, booked) {
   };
 }
 
-
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-
-
+var sunsetHotel = new Hotel('Sunset', 86, 10);
 // Update the HTML for the page
 var details1 = quayHotel.name + ' rooms: ';
     details1 += quayHotel.checkAvailability();
@@ -25,7 +23,12 @@ var details2 = parkHotel.name + ' rooms: ';
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
-/* 
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
+
+/*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
 */
